8c19af38967ac8f426f527a7d84626a47129373c
alejoramon <alejoramon@gmail.com>
2024-01-19 23:59:31 +0100

    queryCategoriesServices

--- src/services/category/categoryQueryService/queryCategoriesService.js
+++ src/services/category/categoryQueryService/queryCategoriesService.js
@@ -1,30 +1,31 @@
 //MÚDULO DE FUNCIONAMIENTO DE SERVICIO DE OBTENCION DEL LISTADO DE CATEGORIAS
 
 // Importamos la bd
-import getPool from '../../../db/getPool.js'; 
-import dotenv from 'dotenv';
+import getPool from "../../../db/getPool.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-//Funcion para obtener lista de categorias 
+//Funcion para obtener lista de categorias
 const fetchCategories = async () => {
-    try {
+  try {
+    let pool = await getPool();
 
-        let pool = await getPool()
+    // Consultamos para obtener categorías
+    const categories = await pool.query(
+      "SELECT id, name FROM categorias ORDER BY name"
+    );
 
-        // Consultamos para obtener categorías 
-        const categories = await pool.query('SELECT id, name FROM categorias');
-        
-        // Verificamos si se encontraron categorías en la bd
-        if (categories && categories.length > 0) {
-            return categories[0];
-        } else {
-            throw new Error("No se encontraron categorías.🔴");
-        }
-    } catch (error) {
-        console.error("Error al obtener categorías:", error);
-        throw new Error("Error interno del servidor al obtener categorías.🔴");
+    // Verificamos si se encontraron categorías en la bd
+    if (categories && categories.length > 0) {
+      return categories[0];
+    } else {
+      throw new Error("No se encontraron categorías.🔴");
     }
+  } catch (error) {
+    console.error("Error al obtener categorías:", error);
+    throw new Error("Error interno del servidor al obtener categorías.🔴");
+  }
 };
 
-export default fetchCategories
+export default fetchCategories;
